7659c857a9485eb242484c16c39b966ac1eed21f
CedricPradels <cedric.pradels@gmail.com>
2020-04-17 09:02:36 +0200

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+interface Categorie {
+  title: string;
+  _id: string;
+}
 
 function App() {
+  const [contexts, setContexts] = useState<Categorie[] | null>(null);
+  useEffect(() => {
+    const fetchContexts = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}context`,
+          { method: "GET" }
+        );
+        if (response.ok) {
+          const datas = await response.json();
+          if (datas.success) setContexts(datas.contexts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchContexts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <button>Agir</button>
+        <button>S'organiser</button>
       </header>
+      <aside>
+        {contexts !== null ? (
+          contexts.map((context) => (
+            <div key={context._id}>{context.title}</div>
+          ))
+        ) : (
+          <div></div>
+        )}
+      </aside>
+      <main></main>
     </div>
   );
 }
