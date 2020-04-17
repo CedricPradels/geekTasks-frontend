7b0ec96b2473a15303524e91953f9151c2f890c3
CedricPradels <cedric.pradels@gmail.com>
2020-04-17 18:37:59 +0200

    Add Context page

--- src/pages/Contexts.tsx
+++ src/pages/Contexts.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Loader from "../components/Loader";
+
+interface Context {
+  _id: string;
+  title: string;
+}
 
 export default function Contexts() {
-  return <></>;
+  const [contexts, setContexts] = useState<null | Context[]>(null);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL_DEV}context`)
+      .then((x) => x.json())
+      .then((y) => setContexts(y.contexts));
+  }, []);
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch(`${process.env.REACT_APP_BACKEND_URL_DEV}context`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title,
+            }),
+          }).then((x) =>
+            fetch(`${process.env.REACT_APP_BACKEND_URL_DEV}context`)
+              .then((x) => x.json())
+              .then((y) => setContexts(y.contexts))
+          );
+        }}
+      >
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="title"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {contexts ? (
+          <>
+            {contexts.map((context) => (
+              <li key={context._id}>{context.title}</li>
+            ))}
+          </>
+        ) : (
+          <Loader />
+        )}
+      </ul>
+    </>
+  );
 }
